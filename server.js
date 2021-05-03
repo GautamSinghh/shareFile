cfb42705ef6e6ed698595d040ecb25cc4a320920
GautamSinghh <gautamsingh@vocvision.com>
2021-05-03 14:02:55 +0530

    add cors

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
 const PORT = process.env.PORT || 4000;
 
 
 app.use(express.static('public'));
 app.use(express.json());
-
+1
 const connectDB = require('./config/db');
 connectDB();
+
+// cors//
+
+const corsOptions = {
+    origin: process.env.ALLOWED_CLIENTS.split(',')
+}
+
+app.use(cors(corsOptions));
+
 //template engine
 app.set('views',path.join(__dirname, './views')); 
 app.set('view engine', 'ejs');
